6dceb7e700552a296979bce725dd3f1cc2c6d6c4
Aaron R Robinson <arobins@microsoft.com>
2023-03-30 20:21:48 -0700

    Additional fixes

--- src/mddump/main.cpp
+++ src/mddump/main.cpp
@@ -297,13 +297,17 @@ bool get_metadata_from_pe(malloc_span<uint8_t>& b)
     if (dos_header->e_lfanew > b.size())
         return false;
 
-    WORD section_header_count = 0;
-    uint8_t* section_header_begin = nullptr;
+    size_t remaining_pe_size = b.size() - dos_header->e_lfanew;
+    uint16_t section_header_count;
+    uint8_t* section_header_begin;
     PIMAGE_NT_HEADERS nt_header_any = (PIMAGE_NT_HEADERS)(b + dos_header->e_lfanew);
     if (nt_header_any->FileHeader.Machine == IMAGE_FILE_MACHINE_AMD64
         || nt_header_any->FileHeader.Machine == IMAGE_FILE_MACHINE_ARM64)
     {
         PIMAGE_NT_HEADERS64 nt_header64 = (PIMAGE_NT_HEADERS64)nt_header_any;
+        if (remaining_pe_size < sizeof(*nt_header64))
+            return false;
+        remaining_pe_size -= sizeof(*nt_header64);
         section_header_count = nt_header64->FileHeader.NumberOfSections;
         section_header_begin = (uint8_t*)&nt_header64[1];
         dotnet_dir = &nt_header64->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR];
@@ -312,6 +316,9 @@ bool get_metadata_from_pe(malloc_span<uint8_t>& b)
             || nt_header_any->FileHeader.Machine == IMAGE_FILE_MACHINE_ARM)
     {
         PIMAGE_NT_HEADERS32 nt_header32 = (PIMAGE_NT_HEADERS32)nt_header_any;
+        if (remaining_pe_size < sizeof(*nt_header32))
+            return false;
+        remaining_pe_size -= sizeof(*nt_header32);
         section_header_count = nt_header32->FileHeader.NumberOfSections;
         section_header_begin = (uint8_t*)&nt_header32[1];
         dotnet_dir = &nt_header32->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR];
@@ -328,9 +335,10 @@ bool get_metadata_from_pe(malloc_span<uint8_t>& b)
         return false;
 
     // Compute the maximum space in the PE to validate section header count.
-    size_t remaining = b.size() - (section_header_begin - b);
-    if (remaining / sizeof(IMAGE_SECTION_HEADER) < section_header_count)
+    if (section_header_count > (remaining_pe_size / sizeof(IMAGE_SECTION_HEADER)))
         return false;
+
+    remaining_pe_size -= section_header_count * sizeof(IMAGE_SECTION_HEADER);
 
     section_headers = { (PIMAGE_SECTION_HEADER)section_header_begin, section_header_count };
 
