1eb07d7f1b1aea83f3afd76a270875ab299d7055
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-07-28 16:56:35 -0700

    Use unique_ptr and remove unused header.

--- src/interfaces/controllingiunknown.hpp
+++ src/interfaces/controllingiunknown.hpp
@@ -12,18 +12,22 @@ class ControllingIUnknown final : public IUnknown
 {
 private:
     std::atomic<int32_t> _refCount{ 1 };
-    std::vector<TearOffUnknown*> _tearOffs;
+    std::vector<std::unique_ptr<TearOffUnknown>> _tearOffs;
 public:
     ControllingIUnknown() = default;
 
     template<typename T, typename... Ts>
     bool CreateAndAddTearOff(Ts... args)
     {
-        T* tearOff = new (std::nothrow) T(this, std::forward<Ts>(args)...);
-        if (tearOff == nullptr)
+        try
+        {
+            _tearOffs.push_back(std::make_unique<T>(this, std::forward<Ts>(args)...));
+            return true;
+        }
+        catch(const std::bad_alloc&)
+        {
             return false;
-        _tearOffs.push_back(tearOff);
-        return true;
+        }
     }
 
 public: // IUnknown
@@ -41,7 +45,7 @@ public: // IUnknown
             return S_OK;
         }
 
-        for (TearOffUnknown* tearOff: _tearOffs)
+        for (std::unique_ptr<TearOffUnknown> const& tearOff: _tearOffs)
         {
             if (tearOff->TryGetInterfaceOnThis(riid, ppvObject) == S_OK)
             {
@@ -64,10 +68,6 @@ public: // IUnknown
         uint32_t c = --_refCount;
         if (c == 0)
         {
-            for (TearOffUnknown* tearOff: _tearOffs)
-            {
-                delete tearOff;
-            }
             delete this;
         }
         return c;
