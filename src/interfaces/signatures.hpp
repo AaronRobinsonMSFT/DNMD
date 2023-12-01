a0ae760d61e91f4c8e39e1a8ec054a1561305e69
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-12-01 15:08:09 -0800

    Add include guard

--- src/interfaces/signatures.hpp
+++ src/interfaces/signatures.hpp
@@ -1,3 +1,5 @@
+#ifndef _SRC_INTERFACES_SIGNATURES_HPP_
+#define _SRC_INTERFACES_SIGNATURES_HPP_
 
 #include <internal/dnmd_platform.hpp>
 #include <internal/span.hpp>
@@ -7,3 +9,5 @@
 #include <cstdint>
 
 malloc_span<uint8_t> GetMethodDefSigFromMethodRefSig(span<uint8_t> methodRefSig);
+
+#endif // _SRC_INTERFACES_SIGNATURES_HPP_
