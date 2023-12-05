5c737739d942da0fe6353f2331fdacf929d7d42e
Aaron R Robinson <arobins@microsoft.com>
2023-12-05 11:27:23 -0800

    Fix unused warnings

--- test/regnative/unit_corsym.cpp
+++ test/regnative/unit_corsym.cpp
@@ -3,6 +3,8 @@
 EXPORT
 TestResult UnitSymReaderAPIs(void const* data, uint32_t dataLen)
 {
+    UNREFERENCED_PARAMETER(data);
+    UNREFERENCED_PARAMETER(dataLen);
     BEGIN_TEST()
     END_TEST()
 }
