246ab6e740374eebc992d5f471c0e1b8617576a9
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-12-13 15:28:27 -0800

    Add tool to locate CoreCLR

---
+++ test/Regression.CoreClrLocator/Program.cs
@@ -0,0 +1,2 @@
+﻿// Output the location of CoreCLR so we can locate it in the tests.
+Console.WriteLine(Path.Combine(Path.GetDirectoryName(typeof(object).Assembly.Location!)!, OperatingSystem.IsWindows() ? "coreclr.dll" : "libcoreclr.so"));
