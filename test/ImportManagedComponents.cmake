914d4cfcd40bbbb4b086b72bdd79ec091c62e602
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-12-20 14:31:48 -0800

    Use DNNE to make a native entrypoint for tests to use for locating managed assets

---
+++ test/ImportManagedComponents.cmake
@@ -0,0 +1,28 @@
+execute_process(
+    COMMAND dotnet build -c Debug
+    COMMAND dotnet build -c Release
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+)
+
+add_library(Regression.Locator IMPORTED SHARED)
+
+if (WIN32)
+    set_target_properties(Regression.Locator PROPERTIES
+        IMPORTED_LOCATION_DEBUG ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/debug/Regression.LocatorNE.dll
+        IMPORTED_LOCATION_RELEASE ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/release/Regression.LocatorNE.dll
+        IMPORTED_IMPLIB_DEBUG ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/debug/Regression.LocatorNE.lib
+        IMPORTED_IMPLIB_RELEASE ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/release/Regression.LocatorNE.lib)
+elseif (APPLE)
+    set_target_properties(Regression.Locator PROPERTIES
+        IMPORTED_LOCATION_DEBUG ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/debug/libRegression.LocatorNE.dylib
+        IMPORTED_LOCATION_RELEASE ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/release/libRegression.LocatorNE.dylib)
+else()
+    set_target_properties(Regression.Locator PROPERTIES
+        IMPORTED_LOCATION_DEBUG ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/debug/libRegression.LocatorNE.so
+        IMPORTED_LOCATION_RELEASE ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/release/libRegression.LocatorNE.so)
+endif()
+
+set_target_properties(Regression.Locator PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/$<LOWER_CASE:$<CONFIG>>/)
+
+set(RegressionLocatorDirectory ${CMAKE_BINARY_DIR}/managed/bin/Regression.Locator/$<CONFIG>/)
--- test/regtest/main.cpp
+++ test/regtest/main.cpp
@@ -4,6 +4,11 @@
 #include "fixtures.h"
 #include "pal.hpp"
 
+#ifdef _WIN32
+#define DNNE_API_OVERRIDE __declspec(dllimport)
+#endif
+#include <Regression.LocatorNE.h>
+
 namespace TestBaseline
 {
     dncp::com_ptr<IMetaDataDispenser> Metadata = nullptr;
@@ -25,47 +30,45 @@ class ThrowListener final : public testing::EmptyTestEventListener {
   }
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
 
-    for (int i = 0; i < argc; ++i) {
-        if (std::string(argv[i]) == "--md-baseline" && i + 1 < argc)
-        {
-            std::string path { argv[++i] };
-            SetBaselineModulePath(path);
-            auto mod = LoadModule(path.c_str());
-            if (mod == nullptr)
-            {
-                std::cout << "Failed to load metadata baseline module: " << argv[i] << std::endl;
-                return -1;
-            }
-
-            auto getDispenser = (MetaDataGetDispenser)GetSymbol(mod, "MetaDataGetDispenser");
-            if (getDispenser == nullptr)
-            {
-                std::cout << "Failed to find MetaDataGetDispenser in module: " << argv[i] << std::endl;
-                return -1;
-            }
-
-            RETURN_IF_FAILED(getDispenser(CLSID_CorMetaDataDispenser, IID_IMetaDataDispenser, (void**)&TestBaseline::Metadata));
-        }
-        else if (std::string(argv[i]) == "--regression-asm" && i + 1 < argc)
-        {
-            SetRegressionAssemblyPath(argv[++i]);
-        }
-    }
-
-    if (TestBaseline::Metadata == nullptr)
+    dncp::cotaskmem_ptr<char> coreClrPath{ (char*)GetCoreClrPath() };
+    if (coreClrPath == nullptr)
     {
-        std::cout << "No metadata baseline module specified" << std::endl;
+        std::cout << "Failed to get coreclr path" << std::endl;
         return -1;
     }
 
-    if (GetRegressionAssemblyMetadata().size() == 0)
+    auto mod = LoadModule(coreClrPath.get());
+    if (mod == nullptr)
     {
-        std::cout << "Invalid regression assembly metadata specified" << std::endl;
+        std::cout << "Failed to load metadata baseline module: " << coreClrPath.get() << std::endl;
         return -1;
     }
+
+    auto getDispenser = (MetaDataGetDispenser)GetSymbol(mod, "MetaDataGetDispenser");
+    if (getDispenser == nullptr)
+    {
+        std::cout << "Failed to find MetaDataGetDispenser in module: " << coreClrPath.get() << std::endl;
+        return -1;
+    }
+
+    RETURN_IF_FAILED(getDispenser(CLSID_CorMetaDataDispenser, IID_IMetaDataDispenser, (void**)&TestBaseline::Metadata));
+
+    std::cout << "Loaded metadata baseline module: " << coreClrPath.get() << std::endl;
+
+    dncp::cotaskmem_ptr<char> regressionAssemblyPath{ (char*)GetRegressionTargetAssemblyPath() };
+    if (regressionAssemblyPath == nullptr)
+    {
+        std::cout << "Failed to get regression assembly path" << std::endl;
+        return -1;
+    }
+
+    SetRegressionAssemblyPath(regressionAssemblyPath.get());
+
+    std::cout << "Regression assembly path: " << regressionAssemblyPath.get() << std::endl;
 
     testing::UnitTest::GetInstance()->listeners().Append(new ThrowListener);
 
