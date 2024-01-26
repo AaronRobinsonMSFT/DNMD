b65cda2f7ba03b39deb304cd4ec4f7eb1be85057
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-01-26 15:35:16 -0800

    Add tests for some corner case scenarios.

--- test/emit/typeref.cpp
+++ test/emit/typeref.cpp
@@ -11,18 +11,23 @@ using WSTR_string = std::wstring;
 using WSTR_string = std::u16string;
 #endif
 
-template<typename T>
-using static_char_buffer = std::array<T, 64>;
-
-TEST(TypeRef, DefineTypeRefWithScopeAndName)
+namespace
 {
-    dncp::com_ptr<IMetaDataDispenser> dispenser;
-    ASSERT_EQ(S_OK, GetDispenser(IID_IMetaDataDispenser, (void**)&dispenser));
+    void CreateEmit(dncp::com_ptr<IMetaDataEmit>& emit)
+    {
+        dncp::com_ptr<IMetaDataDispenser> dispenser;
+        ASSERT_EQ(S_OK, GetDispenser(IID_IMetaDataDispenser, (void**)&dispenser));
+        ASSERT_EQ(S_OK, dispenser->DefineScope(CLSID_CorMetaDataRuntime, 0, IID_IMetaDataEmit, (IUnknown**)&emit));
+    }
+}
+
+TEST(TypeRef, ValidScopeAndDottedName)
+{
     dncp::com_ptr<IMetaDataEmit> emit;
-    ASSERT_EQ(S_OK, dispenser->DefineScope(CLSID_CorMetaDataRuntime, 0, IID_IMetaDataEmit, (IUnknown**)&emit));
+    ASSERT_NO_FATAL_FAILURE(CreateEmit(emit));
     mdTypeRef typeRef;
     WSTR_string name = W("System.Object");
-    ASSERT_EQ(S_OK, emit->DefineTypeRefByName(TokenFromRid(1, mdtModule), W("System.Object"), &typeRef));
+    ASSERT_EQ(S_OK, emit->DefineTypeRefByName(TokenFromRid(1, mdtModule), name.c_str(), &typeRef));
     ASSERT_EQ(1, RidFromToken(typeRef));
     ASSERT_EQ(mdtTypeRef, TypeFromToken(typeRef));
 
@@ -36,5 +41,34 @@ TEST(TypeRef, DefineTypeRefWithScopeAndName)
     EXPECT_EQ(TokenFromRid(1, mdtModule), resolutionScope);
     EXPECT_EQ(readNameLength, name.size() + 1);
     EXPECT_EQ(name, readName.substr(0, readNameLength - 1));
+}
 
+TEST(TypeRef, InvalidScope)
+{
+    dncp::com_ptr<IMetaDataEmit> emit;
+    ASSERT_NO_FATAL_FAILURE(CreateEmit(emit));
+    mdTypeRef typeRef;
+    ASSERT_EQ(E_FAIL, emit->DefineTypeRefByName(TokenFromRid(1, mdtTypeDef), W("System.Object"), &typeRef));
+}
+
+TEST(TypeRef, ValidScopeAndNonDottedName)
+{
+    dncp::com_ptr<IMetaDataEmit> emit;
+    ASSERT_NO_FATAL_FAILURE(CreateEmit(emit));
+    mdTypeRef typeRef;
+    WSTR_string name = W("Bar");
+    ASSERT_EQ(S_OK, emit->DefineTypeRefByName(TokenFromRid(1, mdtModule), name.c_str(), &typeRef));
+    ASSERT_EQ(1, RidFromToken(typeRef));
+    ASSERT_EQ(mdtTypeRef, TypeFromToken(typeRef));
+
+    dncp::com_ptr<IMetaDataImport> import;
+    ASSERT_EQ(S_OK, emit->QueryInterface(IID_IMetaDataImport, (void**)&import));
+    mdToken resolutionScope;
+    WSTR_string readName;
+    readName.resize(name.capacity() + 1);
+    ULONG readNameLength;
+    ASSERT_EQ(S_OK, import->GetTypeRefProps(typeRef, &resolutionScope, readName.data(), (ULONG) readName.size(), &readNameLength));
+    EXPECT_EQ(TokenFromRid(1, mdtModule), resolutionScope);
+    EXPECT_EQ(readNameLength, name.size() + 1);
+    EXPECT_EQ(name, readName.substr(0, readNameLength - 1));
 }
