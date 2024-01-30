3c7571565fed0a93d5eb548d717ed2dcbb0be8f0
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-01-29 16:35:54 -0800

    Add test for SetTypeDefProps

--- test/emit/typedef.cpp
+++ test/emit/typedef.cpp
@@ -156,3 +156,48 @@ TEST(TypeDef, NestedDefine)
     ASSERT_EQ(S_OK, import->GetNestedClassProps(typeDef, &enclosing));
     EXPECT_EQ(outerTypeDef, enclosing);
 }
+
+TEST(TypeDef, SetProps)
+{
+    WSTR_string name = W("Foo");
+    dncp::com_ptr<IMetaDataEmit> emit;
+    ASSERT_NO_FATAL_FAILURE(CreateEmit(emit));
+    mdTypeDef typeDef;
+    mdToken initialImplements[] = { TokenFromRid(2, mdtTypeSpec), mdTokenNil };
+    mdToken extends = TokenFromRid(1, mdtTypeRef);
+
+    ASSERT_EQ(S_OK, emit->DefineTypeDef(name.c_str(), 0, mdTypeDefNil, initialImplements, &typeDef));
+
+    mdToken implements[] = { TokenFromRid(4, mdtTypeRef),  mdTokenNil };
+    ASSERT_EQ(S_OK, emit->SetTypeDefProps(typeDef, tdAbstract, extends, implements));
+
+    dncp::com_ptr<IMetaDataImport> import;
+    ASSERT_EQ(S_OK, emit->QueryInterface(IID_IMetaDataImport, (void**)&import));
+
+    WSTR_string readName;
+    readName.resize(name.capacity() + 1);
+    ULONG readNameLength;
+    DWORD typeDefFlags;
+    mdToken readExtends;
+    ASSERT_EQ(S_OK, import->GetTypeDefProps(typeDef, readName.data(), (ULONG)readName.capacity(), &readNameLength, &typeDefFlags, &readExtends));
+    EXPECT_EQ(name, readName.substr(0, readNameLength - 1));
+    EXPECT_EQ(tdAbstract, typeDefFlags);
+    EXPECT_EQ(extends, readExtends);
+
+    HCORENUM hEnum = nullptr;
+    mdInterfaceImpl interfaceImpls[2] = {};
+    ULONG count;
+    ASSERT_EQ(S_OK, import->EnumInterfaceImpls(&hEnum, typeDef, interfaceImpls, 2, &count));
+    ASSERT_EQ(1, count);
+    // We should have created a new entry for the new interface implementation,
+    // not modified the existing one.
+    EXPECT_EQ(TokenFromRid(2, mdtInterfaceImpl), interfaceImpls[0]);
+    EXPECT_EQ(0, interfaceImpls[1]); // The second element should not be touched.
+    import->CloseEnum(hEnum);
+
+    mdTypeDef classType;
+    mdToken interfaceType;
+    ASSERT_EQ(S_OK, import->GetInterfaceImplProps(interfaceImpls[0], &classType, &interfaceType));
+    EXPECT_EQ(typeDef, classType);
+    EXPECT_EQ(implements[0], interfaceType);
+}
